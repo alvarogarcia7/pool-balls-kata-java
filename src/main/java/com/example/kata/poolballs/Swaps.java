e98c679390c52b114ac342038ae3cc42489fcf91
Alvaro Garcia <derecerca@gmail.com>
2017-07-03 20:56:03 +0200

    Red: return the single swap

--- src/main/java/com/example/kata/poolballs/Swaps.java
+++ src/main/java/com/example/kata/poolballs/Swaps.java
@@ -1,10 +1,29 @@
 package com.example.kata.poolballs;
 
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EqualsAndHashCode
+@ToString
 public class Swaps {
+    private final List<Swap> values;
+
+    private Swaps(Swap[] swaps) {
+        this.values = Arrays.asList(swaps);
+    }
+
+    private Swaps() {
+        this.values = Arrays.asList();
+    }
+
     public static Swaps empty() {
         return new Swaps();
+    }
+
+    public static Swaps of(Swap... swaps) {
+        return new Swaps(swaps);
     }
 }
--- src/test/java/com/example/kata/poolballs/TriangleShould.java
+++ src/test/java/com/example/kata/poolballs/TriangleShould.java
@@ -2,14 +2,22 @@ package com.example.kata.poolballs;
 
 import org.junit.Test;
 
+import static com.example.kata.poolballs.PoolBall.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class TriangleShould {
     @Test
     public void get_the_swaps_for_a_correct_triangle() {
-        Triangle triangle = new Triangle(PoolBall.of("Y"));
+        Triangle triangle = new Triangle(of("Y"));
 
         assertThat(triangle.minimumSetOfSwaps(), is(Swaps.empty()));
+    }
+
+    @Test
+    public void get_the_single_swap() {
+        Triangle triangle = new Triangle(of("R"), of("Y"), of("Y"));
+
+        assertThat(triangle.minimumSetOfSwaps(), is(Swaps.of(Swap.of(0, 1))));
     }
 }
