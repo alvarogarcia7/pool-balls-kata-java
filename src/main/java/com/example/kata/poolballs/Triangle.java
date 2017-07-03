ba7e36f3a631eedd863b42cfa6ec95ab88f7b12f
Alvaro Garcia <derecerca@gmail.com>
2017-07-03 20:30:41 +0200

    Green

--- src/main/java/com/example/kata/poolballs/Triangle.java
+++ src/main/java/com/example/kata/poolballs/Triangle.java
@@ -1,7 +1,20 @@
 package com.example.kata.poolballs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Triangle {
+    private List<PoolBall> balls;
+
+    public Triangle() {
+        this.balls = new ArrayList<>();
+    }
+
     public boolean isEmpty() {
-        return true;
+        return balls.isEmpty();
+    }
+
+    public void addBall(PoolBall poolBall) {
+        balls.add(poolBall);
     }
 }
